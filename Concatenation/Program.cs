35c596a382aed2c6e7a8bfbd7e728cc68b807c50
Jason Bock <jason@jasonbock.net>
2018-08-13 11:23:25 -0700

    Doing some string concatenation fun

---
+++ Concatenation/Program.cs
@@ -0,0 +1,10 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Concatenation
+{
+	class Program
+	{
+		static void Main(string[] args) =>
+			BenchmarkRunner.Run<Concatenation30Parts>();
+	}
+}
---
+++ Concatenation/StringCreation.cs
@@ -0,0 +1,20 @@
+﻿using Spackle;
+
+namespace Concatenation
+{
+	internal static class StringCreation
+	{
+		internal static string Create(int length)
+		{
+			var random = new SecureRandom();
+			var data = new char[length];
+
+			for(var i = 0; i < length; i++)
+			{
+				data[i] = (char)random.Next(32, 127);
+			}
+
+			return new string(data);
+		}
+	}
+}
