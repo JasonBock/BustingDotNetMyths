d04b04f883d2ff030606c90acb08f04f4283334c
Jason Bock <jason.r.bock@outlook.com>
2023-11-07 13:43:45 -0600

    Added object stack allocation demo

--- ReferencesAndValues/Program.cs
+++ ReferencesAndValues/Program.cs
@@ -1,4 +1,6 @@
 ﻿using ReferencesAndValues;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 var referenceData1 = new ReferenceData(1);
 var referenceData2 = referenceData1;
@@ -13,3 +15,17 @@ Console.WriteLine($"{nameof(referenceData2)}: {referenceData2.Data}");
 
 Console.WriteLine($"{nameof(valueData1)}: {valueData1.Data}");
 Console.WriteLine($"{nameof(valueData2)}: {valueData2.Data}");
+
+Console.WriteLine();
+
+unsafe
+{
+	var referenceDataTypeHandleValue = typeof(ReferenceData).TypeHandle.Value;
+	var referenceDataSize = Marshal.ReadInt32(referenceDataTypeHandleValue, 4);
+	Console.WriteLine($"Handle size is {referenceDataSize}");
+
+	var referenceDataMemory = stackalloc byte[referenceDataSize];
+	*(IntPtr*)referenceDataMemory = referenceDataTypeHandleValue;
+	var referenceOnStack = Unsafe.AsRef<ReferenceData>(referenceDataMemory);
+	Console.WriteLine(referenceOnStack.Data);
+}
