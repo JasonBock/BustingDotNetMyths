23cc999216f60306e3a9c7ee78c6bc30324ce960
Jason Bock <jason.r.bock@outlook.com>
2024-04-04 13:34:52 -0500

    Added GotoWalker

---
+++ GotosInRoslyn/GotoWalker.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace GotosInRoslyn;
+
+internal sealed class GotoWalker
+	: CSharpSyntaxWalker
+{
+	public override void Visit(SyntaxNode? node)
+	{
+		var kind = node?.Kind() ?? SyntaxKind.None;
+
+		if(kind != SyntaxKind.None)
+		{
+			if (kind == SyntaxKind.BreakStatement || kind == SyntaxKind.ContinueStatement ||
+				kind == SyntaxKind.CheckedStatement || kind == SyntaxKind.DoStatement ||
+				kind == SyntaxKind.EmptyStatement || kind == SyntaxKind.ExpressionStatement ||
+				kind == SyntaxKind.FixedStatement || kind == SyntaxKind.ForEachStatement ||
+				kind == SyntaxKind.ForEachVariableStatement || kind == SyntaxKind.ForStatement ||
+				kind == SyntaxKind.GlobalStatement || kind == SyntaxKind.GotoCaseStatement ||
+				kind == SyntaxKind.GotoDefaultStatement || kind == SyntaxKind.GotoStatement ||
+				kind == SyntaxKind.IfStatement || kind == SyntaxKind.LabeledStatement ||
+				kind == SyntaxKind.LocalDeclarationStatement || kind == SyntaxKind.LocalFunctionStatement ||
+				kind == SyntaxKind.LockStatement || kind == SyntaxKind.ReturnStatement ||
+				kind == SyntaxKind.SwitchStatement || kind == SyntaxKind.ThrowStatement ||
+				kind == SyntaxKind.TryStatement || kind == SyntaxKind.UncheckedStatement ||
+				kind == SyntaxKind.UnsafeStatement || kind == SyntaxKind.UsingStatement ||
+				kind == SyntaxKind.WhileStatement || kind == SyntaxKind.YieldBreakStatement ||
+				kind == SyntaxKind.YieldReturnStatement)
+			{
+				this.StatementCount++;
+			}
+
+			if (kind == SyntaxKind.GotoStatement)
+			{
+				this.GotoStatementCount++;
+			}
+			else if (kind == SyntaxKind.GotoCaseStatement)
+			{
+				this.GotoCaseStatementCount++;
+			}
+			else if (kind == SyntaxKind.GotoDefaultStatement)
+			{
+				this.GotoDefaultStatementCount++;
+			}
+		}
+
+		base.Visit(node);
+	}
+
+	public int StatementCount { get; private set; }
+	public int GotoStatementCount { get; private set; }
+	public int GotoCaseStatementCount { get; private set; }
+	public int GotoDefaultStatementCount { get; private set; }
+}
---
+++ GotosInRoslyn/Program.cs
@@ -0,0 +1,30 @@
+﻿using GotosInRoslyn;
+using Microsoft.CodeAnalysis.CSharp;
+
+var statementCount = 0;
+var gotoStatementCount = 0;
+var gotoCaseStatementCount = 0;
+var gotoDefaultStatementCount = 0;
+
+// Set directory to the location that contains the Roslyn repo
+var directory = new DirectoryInfo(@"C:\Users\jason\source\repos\dotnet\roslyn");
+
+foreach (var file in directory.EnumerateFiles("*.cs", SearchOption.AllDirectories))
+{
+	Console.WriteLine($"Analyzing {file.FullName}...");
+
+	var tree = CSharpSyntaxTree.ParseText(File.ReadAllText(file.FullName));
+	var walker = new GotoWalker();
+	walker.Visit(tree.GetRoot());
+
+	statementCount += walker.StatementCount;
+	gotoStatementCount += walker.GotoStatementCount;
+	gotoCaseStatementCount += walker.GotoCaseStatementCount;
+	gotoDefaultStatementCount += walker.GotoDefaultStatementCount;
+}
+
+Console.WriteLine();
+Console.WriteLine($"Total statement count: {statementCount}");
+Console.WriteLine($"Total goto statement count: {gotoStatementCount}");
+Console.WriteLine($"Total goto case statement count: {gotoCaseStatementCount}");
+Console.WriteLine($"Total goto default statement count: {gotoDefaultStatementCount}");
